61ccb4007aa6c694cd1c2d9b90693b92a9e0de31
Suraj Kumar <surajkumarok20@gmail.com>
2024-05-17 17:31:16 +0530

    Array

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+       
+        int array[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};   
+    
+       cout<<"first index value is "<<array[0]<<endl;
+
+    return 0;
+}
